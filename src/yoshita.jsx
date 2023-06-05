7dff3a0f1ce97f547726ab1d1420719f29017bb7
Roopesh127 <roopeshvish2006@gmmail.com>
2023-06-05 08:35:47 +0530

    first coomit

---
+++ src/yoshita.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function vpppp(){
+    var x = 10;
+    function updatestate(){
+
+    }
+    return(
+        <>
+        <h1>Learning State{x}</h1>
+        <button onClick = {updatestate}>Click Me</button>
+        </>
+    );
+}
+export default vpppp;
